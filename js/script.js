6902c84fe2acfb7382af9f260ec4c3c9db1867aa
paulhardwork <pavyarin@yandex.ru>
2018-07-05 02:07:25 +0700

    Мелкие правки

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var writeUs = document.querySelector(".btn-write-us");
 var modal = document.querySelector(".modal-write-us");
-var close = document.querySelectorAll(".modal .close-btn");
+var close = modal.querySelector(".close-btn");
 
 writeUs.addEventListener("click", function(appearModal) {
     appearModal.preventDefault();
     modal.classList.add("modal-show");
 });
 
-close.addEventListener("click", function(appearModal) {
-    appearModal.preventDefault();
+close.addEventListener("click", function(disappearModal) {
+    disappearModal.preventDefault();
     modal.classList.remove("modal-show");
 });
